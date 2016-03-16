d14080b2300388c63469f9738ac9971366529cde
Reed <reedsilverstein@gmail.com>
2016-03-15 18:48:49 -0700

    Neurons charge can no longer go below 0

--- neuron.js
+++ neuron.js
@@ -43,6 +43,11 @@ class Neuron {
 	addCharge(charge) {
 		this.curCharge += charge;
 
+    // TESTING
+    if (this.curCharge < 0) {
+      this.curCharge = 0;
+    }
+
 		if (this.curCharge >= this.activationLevel) {
 			this.curCharge = 0;
 			this.activate();
